4899a3d90d9c1860ebd3cfc94e5bd88459d3690a
haldaniko <haldaniko@gmail.com>
2025-07-30 14:21:19 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,20 +3,25 @@
 document.addEventListener('click', (e) => {
   const wall = document.querySelector('.wall');
   const spider = document.querySelector('.spider');
-
   const wallRect = wall.getBoundingClientRect();
-  const spiderRect = spider.getBoundingClientRect();
 
   const spiderHalfWidth = spider.offsetWidth / 2;
   const spiderHalfHeight = spider.offsetHeight / 2;
 
-  let left = e.clientX - wallRect.left - spiderHalfWidth;
-  let top = e.clientY - wallRect.top - spiderHalfHeight;
+  let pageLeft = e.clientX - wallRect.left - spiderHalfWidth;
+  let pageTop = e.clientY - wallRect.top - spiderHalfHeight;
 
-  left = Math.max(0, Math.min(left, wall.clientWidth - spider.offsetWidth));
-  top = Math.max(0, Math.min(top, wall.clientHeight - spider.offsetHeight));
+  pageLeft = Math.max(
+    0,
+    Math.min(pageLeft, wall.clientWidth - spider.offsetWidth),
+  );
+
+  pageTop = Math.max(
+    0,
+    Math.min(pageTop, wall.clientHeight - spider.offsetHeight),
+  );
 
   spider.style.position = 'absolute';
-  spider.style.left = `${left}px`;
-  spider.style.top = `${top}px`;
+  spider.style.left = `${pageLeft}px`;
+  spider.style.top = `${pageTop}px`;
 });
